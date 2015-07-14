ae463916c6c19533183b8ef1a56ff17307016e58
Coderica <esm1018@gmail.com>
2015-07-13 17:15:37 -0700

    changed and fixed color picker functionality

--- js/photopop.js
+++ js/photopop.js
@@ -2,29 +2,49 @@
 $(document).on("pagecreate", "#photo", function() {
 	$('#album').on('click', getAlbum); //go to native photo album
 	$('#camera').on('click', getCamera); //go to native camera
-	// $('#camera').on('click', function(){ goToPage("#doodle") });
+	// $('#camera').on('click', function(){ window.location.href = "#doodle" });
 });
 
 $(document).on("pagecreate", "#doodle", function() {
 	newCanvas();
-	$(".palette").on("click", updatePalette);
 	$("#clear").on("click", newCanvas);
 	$("#share").on("click", function(){ share(imgSrc) });
-	$('#restart').on("click", function(){ goToPage("#photo") })
-	$("#color").colorPicker();
+	$('#restart').on("click", restart)
+
+	// how can I refactor this?
+	$("#color").colorPicker({
+		renderCallback: function($elm, toggled) {
+	    var color = "#" + this.color.colors.HEX
+	    updatePalette(color)
+	  }
+	});
 });
 
+///////////////////////////// PROGRAM
+var context;
+var imgSrc;
 
-///////////////////////////// FUNCTIONS
 function getCamera() {
-	navigator.camera.getPicture(onSuccess, onFail, cameraOptions);
+	navigator.camera.getPicture(
+		onSuccess, 
+		onFail, { 
+		quality: 100,
+	  destinationType: Camera.DestinationType.FILE_URI,
+	  sourceType: Camera.PictureSourceType.CAMERA,
+	  allowEdit: true, //iOS only
+	  encodingType: Camera.EncodingType.JPEG,
+	  saveToPhotoAlbum: true,
+	});
 };
 function getAlbum() {
-	window.imagePicker.getPictures(onSuccess, onFail, albumOptions);
+	window.imagePicker.getPictures(
+		onSuccess, 
+		onFail, 
+		{ maximumImagesCount: 1 }
+	);
 };
-
 // document.addEventListener("deviceready", function () {
-
+	
 //     var options = {
 //       quality: 50,
 //       destinationType: Camera.DestinationType.DATA_URL,
@@ -52,19 +72,21 @@ function getAlbum() {
 
 //   }, false);
 
-function onSuccess(imageData) {
+function onSuccess(imageData){
 	imgSrc = imageData;
 	alert(imageData);
-	goToPage("#doodle");
+	window.location.href = "#doodle";
 };
-function onFail(message) {
+function onFail(message){
 	alert(message);
 };
-function goToPage(newPage) {
-	window.location.href = newPage;
+function restart(){
+	window.location.hash = "#page";
+	location.reload();
 };
-function updatePalette() {
-	color = $("#color").css("background-color");
+function updatePalette(color){
+	console.log(color)
+	$("#color").css("color", color);
 	context.beginPath();
 	context.strokeStyle = color;
 };
@@ -90,11 +112,14 @@ function setupCanvas() {
 };
 function setImageBackground(){
 	var img = new Image();
+	img.src = "http://localhost:8000/css/images/bears.jpg"
+	// img.src = imgSrc;
 	img.onload = function() {
-		context.drawImage(img, 0, 0, $(window).width(), $(window).height());
+		var width = $(window).width()
+		var height = width * this.height/this.width;
+		// crop image height to size of window exempting footer
+		context.drawImage(img, 0, 0, width, height);
 	};
-	// img.src = "http://d.ibtimes.co.uk/en/full/1405855/national-geographic-2014-photo-contest.jpg"
-	img.src = imgSrc;
 };
 // prototype to	start drawing on touch using canvas moveTo and lineTo
 // how can I refactor this???
@@ -103,41 +128,29 @@ $.fn.drawTouch = function() {
     e = e.originalEvent;
 		context.beginPath();
 		x = e.changedTouches[0].pageX;
-		y = e.changedTouches[0].pageY-44;
+		y = e.changedTouches[0].pageY;
 		context.moveTo(x,y);
 	};
 	var move = function(e) {
 		e.preventDefault();
         e = e.originalEvent;
 		x = e.changedTouches[0].pageX;
-		y = e.changedTouches[0].pageY-44;
+		y = e.changedTouches[0].pageY;
 		context.lineTo(x,y);
 		context.stroke();
 	};
 	$(this).on("touchstart", start);
 	$(this).on("touchmove", move);	
 };
-function share(image) {
-	var title = "Made with PhotoPop!"
-	window.plugins.socialsharing.share(null, title, image, null)
+function clearCanvas(){
+	// clear canvas drawing but leave photo
+};
+function saveDrawing(){
+	imgSrc = canvas.toDataURL("image/jpeg");
+	// use on "app close"? background processes?
 }
-
-
-///////////////////////////// VARIABLE SETTINGS
-var cameraOptions = { 
-	quality: 100,
-  destinationType: Camera.DestinationType.FILE_URI, // This kind of path works perfect in the app webview for an img src. EX: "file:///storage/emulated/0/Android/data/com.telerik.CameraTests/cache/resize.jpg?1397645214730".
-  sourceType: Camera.PictureSourceType.CAMERA,
-  allowEdit: true, //iOS only
-  encodingType: Camera.EncodingType.JPEG,
-  saveToPhotoAlbum: true,
-};
-var albumOptions = {
-  maximumImagesCount: 1,
-};
-// var colorPickerOptions = {
-// 	change: function(color){ color.toHexString() },
-// 	renderCallback: function(){ color = this.color },
-// };
-var context, color = "#000";
-var imgSrc
+function share() {
+	saveDrawing();
+	var title = "Made with PhotoPop!"
+	window.plugins.socialsharing.share(null, title, imgSrc, null)
+}
